863f0636f2c3de4788ca6491c3b97301feb5cf41
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-11-03 13:45:07 +0100

    Editable: Remove obsolete valueToElement function and update related tests

--- blocks/api/test/matchers.js
+++ blocks/api/test/matchers.js
@@ -4,22 +4,17 @@
 import { parse } from 'hpq';
 
 /**
- * WordPress dependencies
- */
-import { renderToString } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
-import * as sources from '../matchers';
-import { valueToElement } from '../../editable';
+import * as matchers from '../matchers';
+import { createHTMLFromSimpleNodeList } from '../../api/simple-dom';
 
 describe( 'matchers', () => {
 	const html = '<blockquote><p>A delicious <b>sundae</b> dessert.</p><p>I want it!</p><footer>The Cook</footer></blockquote>';
 
 	describe( 'children()', () => {
 		it( 'should return a source function', () => {
-			const source = sources.children();
+			const source = matchers.children();
 
 			expect( typeof source ).toBe( 'function' );
 		} );
@@ -27,15 +22,15 @@ describe( 'matchers', () => {
 		it( 'should return HTML equivalent WPElement of matched element', () => {
 			// Assumption here is that we can cleanly convert back and forth
 			// between a string and WPElement representation
-			const match = parse( html, sources.children() );
+			const match = parse( html, matchers.children() );
 
-			expect( renderToString( valueToElement( match ) ) ).toBe( html );
+			expect( createHTMLFromSimpleNodeList( match ) ).toBe( html );
 		} );
 	} );
 
 	describe( 'node()', () => {
 		it( 'should return a source function', () => {
-			const source = sources.node();
+			const source = matchers.node();
 
 			expect( typeof source ).toBe( 'function' );
 		} );
@@ -43,10 +38,10 @@ describe( 'matchers', () => {
 		it( 'should return HTML equivalent WPElement of matched element', () => {
 			// Assumption here is that we can cleanly convert back and forth
 			// between a string and WPElement representation
-			const match = parse( html, sources.node() );
+			const match = parse( html, matchers.node() );
 
 			expect(
-				renderToString( valueToElement( [ match ] ) )
+				createHTMLFromSimpleNodeList( [ match ] )
 			).toBe(
 				`<body>${ html }</body>`
 			);
@@ -57,10 +52,10 @@ describe( 'matchers', () => {
 		it( 'should return HTML equivalent WPElement of matched element using selector', () => {
 			// Assumption here is that we can cleanly convert back and forth
 			// between a string and WPElement representation
-			const match = parse( html, sources.query( 'blockquote > p', sources.node( ) ) );
+			const match = parse( html, matchers.query( 'blockquote > p', matchers.node( ) ) );
 
 			expect(
-				renderToString( valueToElement( match ) )
+				createHTMLFromSimpleNodeList( match )
 			).toBe(
 				'<p>A delicious <b>sundae</b> dessert.</p><p>I want it!</p>'
 			);
